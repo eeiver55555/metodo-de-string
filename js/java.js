ae6e785903471f8f4465c46e3d59e364d7b76b58
javier villamil <exeiver55555@gmail.com>
2020-07-08 20:25:36 -0500

    string1

---
+++ js/java.js
@@ -0,0 +1,12 @@
+console.log("hola mundo")
+
+var one = "es una prueba1";
+var two = "es otra prueba2";
+var three = "prueba";
+
+if (one.indexOf(three)!=-1){console.log("esta variable si esta dentro de one")}
+else{console.log("esta varieble no esta en one")}
+
+if(two.indexOf(three)){console.log("esta variable tambien esta dentro de two")}
+else{console.log("esta variable no esta en two")}
+
